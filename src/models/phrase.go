1b91973a0f5bc689f9ae5d08ce8878151b48b0cb
Dialvive <dialvive@protonmail.com>
2021-01-05 11:11:09 -0600

    Fix issues and add postman json

--- src/models/phrase.go
+++ src/models/phrase.go
@@ -7,7 +7,7 @@ type Phrase struct {
 	ID               uint   `json:"ID" gorm:"column:ID;type:INT NOT NULL;primaryKey"`
 	LocaleID         uint   `json:"locale_ID" gorm:"column:locale_ID;type:TINYINT NOT NULL"`
 	PhraseCategoryID uint   `json:"phrase_category_ID" gorm:"column:phrase_category_ID;type:TINYINT NOT NULL"`
-	Text             string `json:"name" gorm:"column:text;type:TEXT NOT NULL"`
+	Text             string `json:"text" gorm:"column:text;type:TEXT NOT NULL"`
 	Context          string `json:"context" gorm:"column:context;type:TEXT"`
 	Modified         string `json:"modified" gorm:"column:modified;type:TIMESTAMP"`
 }
@@ -16,7 +16,7 @@ type Phrase struct {
 type CreatePhraseInput struct {
 	LocaleID         uint   `json:"locale_ID" binding:"required"`
 	PhraseCategoryID uint   `json:"phrase_category_ID" binding:"required"`
-	Text             string `json:"name" binding:"required"`
+	Text             string `json:"text" binding:"required"`
 	Context          string `json:"context" binding:"required"`
 }
 
--- src/models/word.go
+++ src/models/word.go
@@ -7,7 +7,7 @@ type Word struct {
 	ID             uint   `json:"ID" gorm:"column:ID;type:INT NOT NULL;primaryKey"`
 	LocaleID       uint   `json:"locale_ID" gorm:"column:locale_ID;type:TINYINT NOT NULL"`
 	WordCategoryID uint   `json:"word_category_ID" gorm:"column:word_category_ID;type:TINYINT NOT NULL"`
-	Text           string `json:"name" gorm:"column:text;type:TEXT NOT NULL"`
+	Text           string `json:"text" gorm:"column:text;type:TEXT NOT NULL"`
 	Context        string `json:"context" gorm:"column:context;type:TEXT"`
 	Definition     string `json:"definition" gorm:"column:definition;type:TEXT"`
 	Modified       string `json:"modified" gorm:"column:modified;type:TIMESTAMP"`
@@ -16,8 +16,8 @@ type Word struct {
 // CreateWordInput represents a coherent spoken word.
 type CreateWordInput struct {
 	LocaleID       uint   `json:"locale_ID" binding:"required"`
-	WordCategoryID uint   `json:"phrase_category_ID" binding:"required"`
-	Text           string `json:"name" binding:"required"`
+	WordCategoryID uint   `json:"word_category_ID" binding:"required"`
+	Text           string `json:"text" binding:"required"`
 	Definition     string `json:"definition" binding:"required"`
 	Context        string `json:"context" binding:"required"`
 }
