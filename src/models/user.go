2b87592a3756a68a9773dd2c760459036d9aecf4
Brayan Martínez Santana <rojosuper@ciencias.unam.mx>
2021-06-04 11:20:00 -0500

    Adding subscriber type field
    - Now every user has a type: free xor premium
    - Backticks were removed

--- src/models/user.go
+++ src/models/user.go
@@ -4,23 +4,24 @@ import "github.com/dgrijalva/jwt-go"
 
 // User represents a user of Signa Mundi set of services.
 type User struct {
-	ID          uint   `json:"ID" gorm:"column:ID;type:INT NOT NULL;primaryKey"`
-	FirstName   string `json:"first_name" gorm:"column:first_name;type:TEXT"`
-	LastName    string `json:"last_name" gorm:"column:last_name;type:TEXT"`
-	UserName    string `json:"user_name" gorm:"column:user_name;type:VARCHAR(32) NOT NULL"`
-	Mail        string `json:"mail" gorm:"column:mail;type:VARCHAR(254) NOT NULL"`
-	Password    string `json:"password" gorm:"column:password;type:TEXT NOT NULL"`
-	Biography   string `json:"biography" gorm:"column:biography;type:TEXT"`
-	Mailing     string "json:\"mailing\" gorm:\"type:set('`notification`', '`promotion`', '`advertising`');default:'`notification`'\""
-	Privilege   string "json:\"privilege\" gorm:\"type:enum('`child`', '`adult`', '`editor`', '`super user`');default:'`adult`'\""
-	Points      uint   `json:"points" gorm:"column:points;type:INT; default:0"`
-	Credits     uint   `json:"credits" gorm:"column:credits;type:INT; default:0"`
-	LocaleID    uint   `json:"locale_id" gorm:"column:locale_ID;type:INT NOT NULL"`
-	Modified    string `json:"modified" gorm:"column:modified;type:TIMESTAMP"`
-	GoogleSub   string `json:"google_sub,omitempty"`
-	FacebookSub string `json:"facebook_sub,omitempty"`
-	AppleSub    string `json:"apple_sub,omitempty"`
-	Picture     string `json:"picture"`
+	ID             uint   `json:"ID" gorm:"column:ID;type:INT NOT NULL;primaryKey"`
+	FirstName      string `json:"first_name" gorm:"column:first_name;type:TEXT"`
+	LastName       string `json:"last_name" gorm:"column:last_name;type:TEXT"`
+	UserName       string `json:"user_name" gorm:"column:user_name;type:VARCHAR(32) NOT NULL"`
+	Mail           string `json:"mail" gorm:"column:mail;type:VARCHAR(254) NOT NULL"`
+	Password       string `json:"password" gorm:"column:password;type:TEXT NOT NULL"`
+	Biography      string `json:"biography" gorm:"column:biography;type:TEXT"`
+	Mailing        string `json:"mailing" gorm:"type:set('notification', 'promotion', 'advertising');default:'notification'"`
+	Privilege      string `json:"privilege" gorm:"type:enum('child', 'adult', 'editor', 'super user');default:'adult'"`
+	Points         uint   `json:"points" gorm:"column:points;type:INT; default:0"`
+	Credits        uint   `json:"credits" gorm:"column:credits;type:INT; default:0"`
+	LocaleID       uint   `json:"locale_id" gorm:"column:locale_ID;type:INT NOT NULL"`
+	Modified       string `json:"modified" gorm:"column:modified;type:TIMESTAMP"`
+	GoogleSub      string `json:"google_sub,omitempty"`
+	FacebookSub    string `json:"facebook_sub,omitempty"`
+	AppleSub       string `json:"apple_sub,omitempty"`
+	Picture        string `json:"picture"`
+	SubscriberType string `json:"subscriber_type" gorm:"type:enum('free', 'premium');default:'free'"`
 }
 
 // CreateUserInput represents a user of Signa Mundi set of services.
