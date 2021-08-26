dab772cb4de2f1544d40c38a1082117515221770
Brayan Martínez Santana <rojosuper@ciencias.unam.mx>
2021-08-26 10:34:51 -0500

    refactor: move secure user logic
    we use too many of the same code for cleaning user's fields
    and for secure them, and this commit introduces an Unsafe and
    Safe method to deal with that.

--- src/services/users/userService.go
+++ src/services/users/userService.go
@@ -65,35 +65,69 @@ func (usrService UserService) findByExample(result, example *models.User) error 
 }
 
 func (usrService UserService) save(receiver models.User, omitColumns ...string) (models.HandsAppJWT, error) {
-	t := time.Now().UTC().Format("2006-01-02 15:04:05")
-	hashPassword, err := security.HashPassword(receiver.Password)
-	if err != nil {
+	var err error
+	if receiver.Password, err = security.HashPassword(receiver.Password); err != nil {
 		return models.HandsAppJWT{}, err
 	}
-	user := models.User{
-		ID:             0,
-		FirstName:      security.SecureString(receiver.FirstName),
-		LastName:       security.SecureString(receiver.LastName),
-		UserName:       security.SecureString(security.TrimToLength(receiver.UserName, 30)),
-		Mail:           security.SecureString(security.TrimToLength(receiver.Mail, 252)),
-		Password:       hashPassword,
-		Biography:      security.SecureString(security.TrimToLength(receiver.Biography, 140)),
-		Mailing:        receiver.Mailing,
-		Privilege:      receiver.Privilege,
-		Points:         receiver.Points,
-		Credits:        receiver.Credits,
-		LocaleID:       receiver.LocaleID,
-		Modified:       t,
-		GoogleSub:      security.SecureString(security.TrimToLength(receiver.GoogleSub, 68)),
-		FacebookSub:    security.SecureString(security.TrimToLength(receiver.FacebookSub, 68)),
-		AppleSub:       security.SecureString(security.TrimToLength(receiver.AppleSub, 68)),
-		Picture:        security.SecureString(security.TrimToLength(receiver.Picture, 128)),
-		SubscriberType: receiver.SubscriberType,
-	}
-
+	user := Safe(receiver)
+	user.Modified = time.Now().UTC().Format("2006-01-02 15:04:05")
+	user.ID = 0
+	user.SubscriberType = ""
+	user.Points = 0
+	user.Credits = 0
+	user.Privilege = ""
 	if dbError := models.DB.Omit(omitColumns...).Create(&user); dbError.Error != nil {
 		return models.HandsAppJWT{}, dbError.Error
 	}
 
 	return security.CreateJWT(user)
+}
+
+// Unsafe returns a copy for u with all his strings fields without backticks.
+func Unsafe(u models.User) models.User {
+	return models.User{
+		ID:             u.ID,
+		FirstName:      security.RemoveBackticks(u.FirstName),
+		LastName:       security.RemoveBackticks(u.LastName),
+		UserName:       security.RemoveBackticks(u.UserName),
+		Mail:           security.RemoveBackticks(u.Mail),
+		Password:       u.Password,
+		Biography:      security.RemoveBackticks(u.Biography),
+		Mailing:        u.Mailing,
+		Privilege:      u.Privilege,
+		Points:         u.Points,
+		Credits:        u.Credits,
+		LocaleID:       u.LocaleID,
+		Modified:       u.Modified,
+		GoogleSub:      security.RemoveBackticks(u.GoogleSub),
+		FacebookSub:    security.RemoveBackticks(u.FacebookSub),
+		AppleSub:       security.RemoveBackticks(u.AppleSub),
+		Picture:        security.RemoveBackticks(u.Picture),
+		SubscriberType: u.SubscriberType,
+	}
+}
+
+// Safe returns a copy for u with all his strings fields secure for sql injection but
+// Password, Mailing, Privilege, Modified and SubscriberType.
+func Safe(u models.User) models.User {
+	return models.User{
+		ID:             u.ID,
+		FirstName:      security.SecureString(u.FirstName),
+		LastName:       security.SecureString(u.LastName),
+		UserName:       security.SecureString(security.TrimToLength(u.UserName, 30)),
+		Mail:           security.SecureString(security.TrimToLength(u.Mail, 252)),
+		Password:       u.Password,
+		Biography:      security.SecureString(security.TrimToLength(u.Biography, 140)),
+		Mailing:        u.Mailing,
+		Privilege:      u.Privilege,
+		Points:         u.Points,
+		Credits:        u.Credits,
+		LocaleID:       u.LocaleID,
+		Modified:       u.Modified,
+		GoogleSub:      security.SecureString(security.TrimToLength(u.GoogleSub, 68)),
+		FacebookSub:    security.SecureString(security.TrimToLength(u.FacebookSub, 68)),
+		AppleSub:       security.SecureString(security.TrimToLength(u.AppleSub, 68)),
+		Picture:        security.SecureString(security.TrimToLength(u.Picture, 128)),
+		SubscriberType: u.SubscriberType,
+	}
 }
