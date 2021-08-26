c64d3eea062455a9a5672a9dc6f36681005c80cc
Brayan Martínez Santana <rojosuper@ciencias.unam.mx>
2021-08-26 10:58:41 -0500

    fix: resolve csrf tests
    Improves the tests for csrf token and checks:
    
     - if none auth method is present
     - if the header is not present
     - if the cookie is not present
     - if both are present
    
    And refactor the csrf token for delete user

--- src/controllers/userCtrl_test.go
+++ src/controllers/userCtrl_test.go
@@ -5,6 +5,7 @@ import (
 	"API/security"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,59 +13,77 @@ import (
 )
 
 func init() {
-	gin.SetMode(gin.ReleaseMode)
 	models.ConnectDatabase()
 }
 
 func TestCsrfMiddleware(t *testing.T) {
 	r := gin.New()
 	r.GET("/csrf", CsrfMiddleware)
-	jwtMocked, _ := security.CreateJWT(models.User{})
-	req := httptest.NewRequest("GET", "/csrf", nil)
-	w := httptest.NewRecorder()
+	dummyJWT, _ := security.CreateJWT(models.User{})
 
-	t.Run("header is not present", func(t *testing.T) {
-		r.ServeHTTP(w, req)
-		if w.Code != http.StatusBadRequest {
-			t.Fail()
-		}
-	})
+	emptyRequest := httptest.NewRequest(http.MethodGet, "/csrf", nil)
 
-	req.Header.Set(HandsAppCsrfToken, jwtMocked.CsrfToken)
-	t.Run("cookie is not present", func(t *testing.T) {
-		r.ServeHTTP(w, req)
-		if w.Code != http.StatusBadRequest {
-			t.Fail()
-		}
-	})
+	justHeader := emptyRequest.Clone(emptyRequest.Context())
+	justHeader.Header.Set(HandsAppCsrfToken, dummyJWT.CsrfToken)
 
-	req.Header.Set("Cookie", fmt.Sprint(HandsAppSession, "=", jwtMocked.Token))
-	t.Run("header is not present", func(t *testing.T) {
-		r.ServeHTTP(w, req)
-		if w.Code != http.StatusOK {
-			t.Fail()
-		}
-	})
+	justCookie := emptyRequest.Clone(emptyRequest.Context())
+	justCookie.Header.Set("Cookie", fmt.Sprint(HandsAppSession, "=", dummyJWT.Token))
 
+	headerAndCookie := justHeader.Clone(justHeader.Context())
+	headerAndCookie.Header.Set("Cookie", fmt.Sprint(HandsAppSession, "=", dummyJWT.Token))
+
+	testCases := []struct {
+		Name     string
+		Expected int
+		*http.Request
+	}{
+		{"without authentication", http.StatusBadRequest, emptyRequest},
+		{"header is not present", http.StatusBadRequest, justCookie},
+		{"cookie is not present", http.StatusBadRequest, justHeader},
+		{"should pass when cookie and header are set", http.StatusOK, headerAndCookie},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+
+			r.ServeHTTP(w, tc.Request)
+			if w.Code != tc.Expected {
+				t.Fatal("Expected: ", tc.Expected, "got: ", w.Code)
+			}
+		})
+	}
 }
 
 func TestDeleteUser(t *testing.T) {
 	name := fmt.Sprint(t.Name(), "@", time.Now().Unix())
-	userJWT, _ := userService.SignWithHandsApp(models.User{UserName: name, Mail: name, Password: name, LocaleID: 1})
+	userJWT, err := userService.SignWithHandsApp(models.User{UserName: name, Mail: name, Password: name, LocaleID: 1})
+	if err != nil {
+		t.Fatal("error creating user: ", err.Error())
+	}
+
 	claims := &models.UserClaim{}
 	_ = security.ParseJWT(userJWT.Token, claims)
 
 	r := gin.New()
 	r.DELETE("/delete/:ID", CsrfMiddleware, DeleteUser)
 
-	req := httptest.NewRequest(http.MethodDelete, fmt.Sprintf("/delete/%s", claims.Subject), nil)
-	req.Header.Set(HandsAppCsrfToken, userJWT.CsrfToken)
-	req.Header.Set("Cookie", fmt.Sprint(HandsAppSession, "=", userJWT.Token))
+	req := newRequestUsingCsrfToken(
+		http.MethodDelete,
+		fmt.Sprint("/delete/", claims.Subject),
+		nil,
+		userJWT,
+	)
 	w := httptest.NewRecorder()
-
 	r.ServeHTTP(w, req)
-
 	if w.Code != http.StatusOK {
 		t.Fail()
 	}
+}
+
+func newRequestUsingCsrfToken(method, path string, body io.Reader, userJWT models.HandsAppJWT) *http.Request {
+	req := httptest.NewRequest(method, path, body)
+	req.Header.Set(HandsAppCsrfToken, userJWT.CsrfToken)
+	req.Header.Set("Cookie", fmt.Sprint(HandsAppSession, "=", userJWT.Token))
+	return req
 }
